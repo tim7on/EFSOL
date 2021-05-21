4e4c0651f8100a0bfb6b973c353e43c324541667
Tim <timson995@gmail.com>
2021-05-21 18:07:15 +0600

    Добавьте файлы проекта.

---
+++ lesson5_task3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace lesson5_task3
+{
+    /*
+     * Задание 3 
+    Имеется 3 переменные типа int x = 5, y = 10, и z = 15;
+    Выполните и рассчитайте результат следующих операций для этих переменных: 
+     x += y >> x++ * z; 
+     z = ++x & y * 5; 
+     y /= x + 5 | z; 
+     z = x++ & y * 5; 
+     x = y << x++ ^ z;
+    */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n---------------\nПеременные\nЗадание 1\n");
+            int x = 5, y = 10, z = 15;
+            Console.WriteLine($"\nПеременные до решения: \n x={x}, \t y={y}, \t z={z}\n" +
+                $"Решение для {x += y >> x++ * z} = x += y >> x++ * z\n" +
+                $"Переменные: \n x={x}, \t y={y}, \t z={z}\n");
+            Console.WriteLine($"\nПеременные до решения: \n x={x}, \t y={y}, \t z={z}\n" + 
+                $"Решение для {z = ++x & y * 5} = z = ++x & y * 5\n" +
+                $"Переменные: \n x={x}, \t y={y}, \t z={z}\n");
+            Console.WriteLine($"\nПеременные до решения: \n x={x}, \t y={y}, \t z={z}\n" + 
+                $"Решение для {y /= x + 5 | z} = y /= x + 5 | z\n" +
+                $"Переменные: \n x={x}, \t y={y}, \t z={z}\n");
+            Console.WriteLine($"\nПеременные до решения: \n x={x}, \t y={y}, \t z={z}\n" + 
+                $"Решение для {z = x++ & y * 5} = z = x++ & y * 5\n" +
+                $"Переменные: \n x={x}, \t y={y}, \t z={z}\n");
+            Console.WriteLine($"\nПеременные до решения: \n x={x}, \t y={y}, \t z={z}\n" + 
+                $"Решение для {x = y << x++ ^ z} = x = y << x++ ^ z\n" +
+                $"Переменные: \n x={x}, \t y={y}, \t z={z}\n");
+        }
+    }
+}
---
+++ lesson7_task2/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace lesson7_task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Задание 2
+Используя Visual Studio, создайте проект по шаблону Console Application.
+Создайте четыре метода для выполнения арифметических операций, с именами: Add – сложение, Sub –
+вычитание, Mul – умножение, Div – деление. Каждый метод должен принимать два целочисленных
+аргумента и выводить на экран результат выполнения арифметической операции соответствующей
+имени метода. Метод деления Div, должен выполнять проверку попытки деления на ноль.
+Требуется предоставить пользователю возможность вводить с клавиатуры значения операндов и знак
+арифметической операции, для выполнения вычислений.*/
+            Console.WriteLine("Введите первое число.");
+            double num1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите Второе число.");
+            double num2 = double.Parse(Console.ReadLine());
+            Start:
+                Console.WriteLine("Выберите действие: +, -, /, *");
+                char sign = char.Parse(Console.ReadLine());
+                double result = 0;
+                if (sign == '+')
+                {
+                    result = Add(num1, num2);
+                }
+                else if (sign == '-')
+                {
+                    result = Sub(num1, num2);
+                }
+                else if (sign == '/')
+                {
+                    result = Div(num1, num2);
+                }
+                else if (sign == '*')
+                {
+                    result = Mul(num1, num2);
+                }
+                else
+                {
+                    Console.WriteLine("Выбран неверный знак");
+                    goto Start;
+                }
+
+            Console.WriteLine($"Результат действий {result}");
+
+
+            static double Add(double num1, double num2)
+            {
+                return num1 + num2;
+            }
+            static double Sub(double num1, double num2)
+            {
+                return num1 - num2;
+            }
+            static double Div(double num1, double num2)
+            {
+                double res = 0;
+                if (num2 != 0)
+                {
+                    res = num1 - num2;
+                }
+                else
+                {
+                    Console.WriteLine("Не возмжно поделить на ноль");
+                }
+                return  res;
+            }
+            static double Mul(double num1, double num2)
+            {
+                return num1 * num2;
+            }
+
+        }
+    }
+}
