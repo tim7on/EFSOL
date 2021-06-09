a2a0cedfeb6c3f2659fe4280373496990e847a67
Tim <timson995@gmail.com>
2021-06-09 17:01:52 +0600

    Created pro_12_3

---
+++ pro_12_3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+/*Задание 3 
+Создайте приложение, которое может быть запущено только в одном экземпляре (используя 
+именованный Mutex). */
+namespace pro_12_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
