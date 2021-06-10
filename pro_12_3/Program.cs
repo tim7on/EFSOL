027fae455c0aa684fb0182cefc74f69fd9605064
Tim <timson995@gmail.com>
2021-06-10 23:15:58 +0600

    finished pro 12_3

--- pro_12_3/Program.cs
+++ pro_12_3/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 /*Задание 3 
 Создайте приложение, которое может быть запущено только в одном экземпляре (используя 
 именованный Mutex). */
@@ -6,9 +7,31 @@ namespace pro_12_3
 {
     class Program
     {
+        static Mutex mutexObj = new Mutex(false, "myMutex");
+        static int x = 0;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (int i = 0; i < 5; i++)
+            {
+                Thread myThread = new Thread(Count);
+                myThread.Name = $"Поток {i}";
+                myThread.Start();
+            }
+
+            Console.ReadLine();
+        }
+        public static void Count()
+        {
+            mutexObj.WaitOne();
+            x = 1;
+            for (int i = 1; i < 9; i++)
+            {
+                Console.WriteLine($"{Thread.CurrentThread.Name}: {x}");
+                x++;
+                Thread.Sleep(100);
+            }
+            mutexObj.ReleaseMutex();
         }
     }
 }
