e46f88acdb986154527dfa651330b5c64589b00a
Tim <timson995@gmail.com>
2021-06-11 17:12:43 +0600

    Undone pro_13_2

---
+++ pro_13_2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+// NOT WORK ON THIS PLATFORM
+namespace pro_13_2
+{
+    class program
+    {
+        public delegate void InvokeDelegate();
+        
+        static void Method()
+        {
+            Console.WriteLine("Method starts");
+            Thread.Sleep(2000);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            InvokeDelegate test = Method;
+            Action myDelegate = new Action(Method);
+            myDelegate.BeginInvoke(test, null);
+        }
+    }
+}
